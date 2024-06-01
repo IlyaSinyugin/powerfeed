aa7833c9f32a2bdd0441dc6d90ac0ed37fc856e7
Ilya <ilya@sinyugin.ru>
2024-06-01 23:28:52 +0200

    test quick

--- api/index.tsx
+++ api/index.tsx
@@ -103,6 +103,107 @@ app.frame("/score/:id", neynarMiddleware, async (c) => {
   let username, score;
 
   // if fid is undefined, then display the default image 
+  if (!fid) {
+    console.log("FID is undefined");
+    // return the data related to the hash
+    const hashData = await sql`
+      SELECT username, pfpurl, fid, score
+      FROM user_scores
+      WHERE hash = ${hash}
+    `;
+
+    if (hashData.rows.length > 0) {
+      console.log("Hash exists in the database");
+      ({ username, pfpUrl, fid, score } = hashData.rows[0]);
+      console.log(`fid hash username score ${fid} ${hash} ${username} ${score}`)
+      let shareUrl = `https://warpcast.com/~/compose?text=Check%20your%20Farcaster%20Power%20and%20join%20the%20OᖴᖴᑕᕼᗩIᑎ%20ᔕᑌᗰᗰEᖇ!🏖️&embeds%5B%5D=https://powerfeed.vercel.app/api/score/${hash}`;
+      return c.res({
+        image: (
+          <Rows gap="1" grow>
+            <Row backgroundColor="background" height="2/7" />
+            <Divider color="green" />
+            <Row
+              backgroundColor="background"
+              height="3/7"
+              alignHorizontal="left"
+              alignVertical="center"
+              padding="16"
+            >
+              <HStack gap="18" alignHorizontal="center" alignVertical="center">
+                <img
+                  //src="https://imgur.com/WImxm1D.jpeg"
+                  src={pfpUrl}
+                  width="128"
+                  height="128"
+                  style={{
+                    borderRadius: "0%",
+                    border: "3.5px solid #B1FC5A",
+                  }}
+                />
+                <VStack gap="1">
+                  <Text
+                    color="white"
+                    size="18"
+                    decoration="solid"
+                    weight="800"
+                    wrap="balance"
+                  >
+                    {username}
+                  </Text>
+                  <Text color="green" size="18" decoration="solid" weight="800">
+                    got the power!
+                  </Text>
+                </VStack>
+                <Spacer size="72" />
+                <Box
+                  fontSize="18"
+                  color="white"
+                  fontStyle="JetBrains Mono"
+                  fontFamily="default"
+                  fontWeight="800"
+                  alignContent="center"
+                  alignVertical="center"
+                  paddingBottom="14"
+                  flexWrap="nowrap"
+                  display="flex"
+                >
+                  Power Score: {score}
+                </Box>
+              </HStack>
+            </Row>
+            <Divider color="green" />
+            <Row
+              backgroundColor="background"
+              height="3/7"
+              alignHorizontal="right"
+              paddingLeft="16"
+              paddingRight="16"
+              paddingTop="22"
+              textAlign="center"
+            >
+              <Text color="white" size="18" decoration="solid" weight="800">
+                Power Score = power users engaged with your casts last week. Use it
+                to give and earn $power in the /powerfeed game!
+              </Text>
+            </Row>
+          </Rows>
+        ),
+        intents: [
+          <Button.Link href={shareUrl}>Share</Button.Link>,
+          <Button action={`/score/${hash}`} value="checkScore">
+            Score
+          </Button>,
+          <Button action="/gamerules" value="joinGame">
+            Play
+          </Button>,
+        ],
+      });
+    }
+
+    
+
+    
+  }
 
   // check if fid is already in the dictionary
   if (fidScore[fid]) {
